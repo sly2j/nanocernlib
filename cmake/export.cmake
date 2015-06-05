29ca54018ccad1d7d2ef3e025a6a9054008b96c9
Sylvester Joosten <sylvester.joosten@temple.edu>
2015-06-04 22:23:52 -0400

    renamed miniCernlib minicernlib, renamed projectConfig -> project-config

--- cmake/export.cmake
+++ cmake/export.cmake
@@ -5,38 +5,38 @@
 string(TOUPPER ${PROJECT_NAME} PROJECT_NAME_CAPS)
 ## Add all targets to the build-tree export set
 export(TARGETS ${TARGETS} 
-  FILE "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Targets.cmake")
+  FILE "${PROJECT_BINARY_DIR}/${PROJECT_NAME}-targets.cmake")
 
 ## Export the package for use from the build-tree
 ## (this registers the build-tree with a global CMake-registry)
 export(PACKAGE ${PROJECT_NAME})
 
-## Create the ProjectConfig.cmake and ProjectConfigVersion.cmake files
+## Create the Project-config.cmake and Project-config-version.cmake files
 file(RELATIVE_PATH REL_INCLUDE_DIR "${INSTALL_CMAKE_DIR}"
   "${INSTALL_INCLUDE_DIR}")
-## ... projectConfig.cmake for the build-tree
+## ... project-config.cmake for the build-tree
 set(CONF_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}")
-configure_file("cmake/${PROJECT_NAME}Config.cmake.in"
-  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake" @ONLY)
-## ... projectConfig.cmake for the install-tree
+configure_file("cmake/${PROJECT_NAME}-config.cmake.in"
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config.cmake" @ONLY)
+## ... project-config.cmake for the install-tree
 set(CONF_INCLUDE_DIRS
   "\${${PROJECT_NAME_CAPS}}/${REL_INCLUDE_DIR}")
-configure_file("cmake/${PROJECT_NAME}Config.cmake.in"
-  "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${PROJECT_NAME}Config.cmake"
+configure_file("cmake/${PROJECT_NAME}-config.cmake.in"
+  "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${PROJECT_NAME}-config.cmake"
   @ONLY)
-## ... projectConfigVersion.cmake for both
-configure_file("cmake/${PROJECT_NAME}ConfigVersion.cmake.in"
-  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake" @ONLY)
+## ... project-config-version.cmake for both
+configure_file("cmake/${PROJECT_NAME}-config-version.cmake.in"
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config-version.cmake" @ONLY)
 
 ################################################################################
 ## INSTALL the configuration files
 ################################################################################
-## Install the projectConfig.cmake and projectConfigVersion.cmake files
+## Install the project-config.cmake and project-config-version.cmake files
 install (FILES
-  "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${PROJECT_NAME}Config.cmake"
-  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+  "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${PROJECT_NAME}-config.cmake"
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config-version.cmake"
   DESTINATION ${INSTALL_CMAKE_DIR} COMPONENT dev)
 
 ## Install the export set to be used with the install-tree
-install(EXPORT "${PROJECT_NAME}Targets"
+install(EXPORT "${PROJECT_NAME}-targets"
   DESTINATION ${INSTALL_CMAKE_DIR} COMPONENT dev)
